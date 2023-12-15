6010eea143e784013718e237aa40d5cae5e978be
Duygu Eylül KARATAŞ <96980093+duygueylul@users.noreply.github.com>
2023-12-16 00:49:39 +0300

    Initial commit

---
+++ src/MatrisYaz/Matris.java
@@ -0,0 +1,22 @@
+package MatrisYaz;
+
+public class Matris {
+
+	public static void main(String[] args) {
+		
+		
+		int[][] mtrs = {{51,43,57}, {15,78,39}};
+		
+		
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 3; j++) {
+				System.out.print(mtrs[i][j] + " ");
+			}
+			System.out.println();
+			
+		}
+		
+
+	}
+
+}
